a7fa04e5051055cee7f6c4cc7e56734b4e83dfd0
Jose Gallo <josegallo@gmail.com>
2024-11-07 07:18:27 -0500

    fetch weahter calling API with longitude and altitude parameters

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -9,14 +9,14 @@ const apiKey = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API ke
 
 const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-
 fetch(apiUrl)
 .then(response => response.json())
 .then(data => {
   const weatherInfo = document.getElementById('weatherInfo');
   console.log("data : ")
   console.log(data);
-  weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+  weatherInfo.innerHTML = `<h2>Data using name</h2>
+                           <h2>Weather in ${data.name}</h2>
                            <p>Temperature: ${data.main.temp} &#8451;</p>
                            <p>Weather: ${data.weather[0].description}</p>`;
 })
@@ -30,3 +30,35 @@ fetch(apiUrl)
 }
   
 document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
+
+function showweatherDetails2(event) {
+    event.preventDefault();
+
+const lat = document.getElementById('latitude').value;
+
+const lon = document.getElementById('longitude').value;
+
+const apiKey2 = 'YOUR_API_KEY'; // Replace 'YOUR_API_KEY' with your actual API key
+
+const apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey2}&units=metric`;
+
+fetch(apiUrl2)
+.then(response => response.json())
+.then(data => {
+const weatherInfo = document.getElementById('weatherInfo2');
+console.log("data : ")
+console.log(data);
+weatherInfo.innerHTML = `<h2>Data using long. and lat.</h2>
+                         <h2>Weather in ${data.name}</h2>
+                         <p>Temperature: ${data.main.temp} &#8451;</p>
+                         <p>Weather: ${data.weather[0].description}</p>`;
+})
+.catch(error => {
+  console.error('Error fetching weather:', error);
+  const weatherInfo = document.getElementById('weatherInfo2');
+  weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+});
+
+
+}
+document.getElementById('coordinatesForm').addEventListener('submit',showweatherDetails2 );
